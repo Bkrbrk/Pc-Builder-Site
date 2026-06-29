4777898662f8c4cb1838bae13e3c794268591ef4
Brk <lnrmnc0@gmail.com>
2026-06-29 13:17:22 +0300

    domain: add canonical domain constants

---
+++ backend/src/domain/manufacturers.js
@@ -0,0 +1,33 @@
+// backend/src/domain/manufacturers.js
+// Canonical manufacturer examples. This list is intentionally extensible.
+// Normalizers should map provider manufacturer strings to these canonical names when possible.
+
+export const CANONICAL_MANUFACTURERS = Object.freeze([
+  'AMD',
+  'Intel',
+  'NVIDIA',
+  'ASUS',
+  'MSI',
+  'Gigabyte',
+  'ASRock',
+  'Corsair',
+  'Kingston',
+  'Samsung',
+  'Crucial',
+  'WD',
+  'Seagate',
+  'Cooler Master',
+  'NZXT',
+  'Thermaltake',
+  'be quiet!',
+  'Noctua',
+  'DeepCool',
+  'Arctic',
+  'PowerColor',
+  'Sapphire',
+  'XFX',
+  'ZOTAC',
+  'Palit',
+  'Gainward',
+  'PNY'
+])
